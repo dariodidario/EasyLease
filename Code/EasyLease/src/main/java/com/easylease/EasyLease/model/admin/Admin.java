a0b73b08217cffc3d30e22f405d1d169980d786d
OhhBigg <sarroantonio1999@gmail.com>
2020-12-28 22:37:05 +0100

    Implementazioni Model Pacchetto 1
    Oltre all'implementazione dei model del pacchetto 1(da sistemare quando si hanno a disposizione User e Estimate), ed ho anche implementato le control di utility, insieme ad un'eccezione per il controllo dell'entity.

--- Code/EasyLease/src/main/java/com/easylease/EasyLease/model/admin/Admin.java
+++ Code/EasyLease/src/main/java/com/easylease/EasyLease/model/admin/Admin.java
@@ -1,4 +1,76 @@
 package com.easylease.EasyLease.model.admin;
 
-public class Admin {
+import com.easylease.EasyLease.model.order.Order;
+
+import java.util.Objects;
+
+import static org.graalvm.compiler.options.OptionType.User;
+
+/**
+ * This class models an object of type Admin within the system.
+ *
+ * @since 0.1
+ * @author Antonio Sarro
+ * @version 0.1
+ */
+public class Admin extends User {
+
+  private String newProperty; //TODO: Trovare proprieta da aggiungere
+
+  public Admin() {
+
+  }
+
+  /**
+   * Constructor for the Admin Object.
+   *
+   * @param newProperty newProperty.
+   */
+  public Admin(String newProperty) {
+    super();
+    this.newProperty = newProperty;
+  }
+
+  /**
+   * Returns the newProperty.
+   *
+   * @return newProperty.
+   */
+  public String getNewProperty() {
+    return newProperty;
+  }
+
+  /**
+   * Set the newProperty.
+   *
+   * @param newProperty newProperty.
+   */
+  public void setNewProperty(String newProperty) {
+    this.newProperty = newProperty;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+    Admin admin = (Admin) obj;
+    //return this.id.equals(order.id);
+    return true; //TODO: Cambiare quando avrò tutti i model
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(newProperty); //TODO: Cambiare con id quando ho tutto
+  }
+
+  @Override
+  public String toString() { //TODO: Aggiustare toString quando ho tutto
+    return "Admin{"
+        + "newProperty='" + newProperty + '\''
+        + '}';
+  }
 }
