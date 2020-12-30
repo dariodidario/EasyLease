1921880ffc8fb5209a5fb008cdde5d58b8ab43b6
Francesco Maria Torino <72786173+francescotorino1@users.noreply.github.com>
2020-12-30 23:46:46 +0100

    adattamento user dao

--- Code/EasyLease/src/main/java/com/easylease/EasyLease/model/user/DBUserDAO.java
+++ Code/EasyLease/src/main/java/com/easylease/EasyLease/model/user/DBUserDAO.java
@@ -1,9 +1,16 @@
 package com.easylease.EasyLease.model.user;
 
-import com.easylease.EasyLease.model.DBPool.ConnectionPool;
+import com.easylease.EasyLease.model.DBPool.DBConnection;
+import com.easylease.EasyLease.model.exception.EntityTamperingException;
+import com.easylease.EasyLease.model.order.DBOrderDAO;
+import com.easylease.EasyLease.model.order.Order;
+import com.easylease.EasyLease.model.order.OrderDAO;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class implements the UserDAO interface, using the singleton DBConnection
@@ -16,57 +23,77 @@ import java.util.List;
 
 public class DBUserDAO implements UserDAO{
 
+  private static Logger logger = Logger.getLogger(DBUserDAO.class.getName());
+  private static DBUserDAO dao;
+  private Connection connection;
   private static final String TABLE_NAME = "user";
 
-  public synchronized User retrieveById(String Id) throws SQLException {
-    Connection connection = null;
-    PreparedStatement preparedStatement = null;
+  /**
+   * Returns a DBOrderDAO Singleton Object.
+   *
+   * @return the {@link DBUserDAO} Object that accesses the {@link User} object
+   *      in the Database.
+   */
+  public static DBUserDAO getInstance() {
+    if (dao == null) {
+      dao = new DBUserDAO(DBConnection.getInstance().getConnection());
+    }
+    return dao;
+  }
 
-    User bean = new User();
+  private DBUserDAO(Connection connection) {
+    this.connection = connection;
+  }
 
-    String selectSQL = "SELECT * FROM " + DBUserDAO.TABLE_NAME + " WHERE ID = ?";
-
+  @Override
+  public User retrieveById(String id) {
+    final String query = "SELECT * FROM " + DBUserDAO.TABLE_NAME + " WHERE ID = ?";
+    if (id == null || id.equals("")) {
+      throw new IllegalArgumentException(
+          String.format("The id(%s) passed as a parameter is not valid", id));
+    }
     try {
-      connection = ConnectionPool.getConnection();
-      preparedStatement = connection.prepareStatement(selectSQL);
-      preparedStatement.setString(1, Id);
+      PreparedStatement stm = connection.prepareStatement(query);
+      stm.setString(1, id);
+      stm.execute();
 
-      ResultSet rs = preparedStatement.executeQuery();
+      ResultSet rs = stm.getResultSet();
+      if (!rs.next()) {
+        return null;
+      }
+      User bean = new User();
 
-      while (rs.next()) {
+      while (rs.next()){
         bean.setId(rs.getString("ID"));
         bean.setName(rs.getString("NAME"));
         bean.setSurname(rs.getString("SURNAME"));
         bean.setEmail(rs.getString("EMAIL"));
         bean.setPassword(rs.getString("PASSWORD"));
       }
-
-    } finally {
-      try {
-        if (preparedStatement != null) {
-          preparedStatement.close();
-        }
-      } finally {
-        ConnectionPool.releaseConnection(connection);
-      }
+      return bean;
+    } catch (SQLException ex) {
+      logger.log(Level.SEVERE, ex.getMessage());
+      return null;
     }
-    return bean;
   }
 
-  public synchronized List<User> retrieveByType(Enum tipo) throws SQLException {
-    Connection connection = null;
-    PreparedStatement preparedStatement = null;
-
-    List<User>  users = new ArrayList<User>();
-
-    String selectSQL = "SELECT * FROM " + DBUserDAO.TABLE_NAME + " WHERE TYPE = ?";
-
+  @Override
+  public List<User> retrieveByType(Enum tipo) {
+    final String query = "SELECT * FROM " + DBUserDAO.TABLE_NAME + " WHERE TYPE = ?";
+    if (tipo.name() == null || tipo.name().equals("")) {
+      throw new IllegalArgumentException(
+          String.format("The id(%s) passed as a parameter is not valid", tipo));
+    }
     try {
-      connection = ConnectionPool.getConnection();
-      preparedStatement = connection.prepareStatement(selectSQL);
-      preparedStatement.setString(1, tipo.name());
+      PreparedStatement stm = connection.prepareStatement(query);
+      stm.setString(1, tipo.name());
+      stm.execute();
 
-      ResultSet rs = preparedStatement.executeQuery();
+      ResultSet rs = stm.getResultSet();
+      if (!rs.next()) {
+        return null;
+      }
+      List<User>  users = new ArrayList<User>();
 
       while (rs.next()) {
         User bean = new User();
@@ -78,66 +105,57 @@ public class DBUserDAO implements UserDAO{
         bean.setPassword(rs.getString("PASSWORD"));
         users.add(bean);
       }
-
-    } finally {
-      try {
-        if (preparedStatement != null) {
-          preparedStatement.close();
-        }
-      } finally {
-        ConnectionPool.releaseConnection(connection);
-      }
+      return users;
+    } catch (SQLException ex) {
+      logger.log(Level.SEVERE, ex.getMessage());
+      return null;
     }
-    return users;
   }
 
-  public synchronized User retrieveByEmail(String Email) throws SQLException {
-    Connection connection = null;
-    PreparedStatement preparedStatement = null;
-
-    User bean = new User();
-
-    String selectSQL = "SELECT * FROM " + DBUserDAO.TABLE_NAME + " WHERE EMAIL = ?";
-
+  @Override
+  public User retrieveByEmail(String Email)  {
+    final String query = "SELECT * FROM " + DBUserDAO.TABLE_NAME + " WHERE EMAIL = ?";
+    if (Email == null || Email.equals("")) {
+      throw new IllegalArgumentException(
+          String.format("The id(%s) passed as a parameter is not valid", Email));
+    }
     try {
-      connection = ConnectionPool.getConnection();
-      preparedStatement = connection.prepareStatement(selectSQL);
-      preparedStatement.setString(1, Email);
+      PreparedStatement stm = connection.prepareStatement(query);
+      stm.setString(1, Email);
+      stm.execute();
 
-      ResultSet rs = preparedStatement.executeQuery();
+      ResultSet rs = stm.getResultSet();
+      if (!rs.next()) {
+        return null;
+      }
+      User bean = new User();
 
-      while (rs.next()) {
+      while (rs.next()){
         bean.setId(rs.getString("ID"));
         bean.setName(rs.getString("NAME"));
         bean.setSurname(rs.getString("SURNAME"));
         bean.setEmail(rs.getString("EMAIL"));
         bean.setPassword(rs.getString("PASSWORD"));
       }
-
-    } finally {
-      try {
-        if (preparedStatement != null)
-          preparedStatement.close();
-      } finally {
-        ConnectionPool.releaseConnection(connection);
-      }
+      return bean;
+    } catch (SQLException ex) {
+      logger.log(Level.SEVERE, ex.getMessage());
+      return null;
     }
-    return bean;
   }
 
-  public synchronized List<User> retrieveAll() throws SQLException {
-    Connection connection = null;
-    PreparedStatement preparedStatement = null;
-
-    List<User>  users = new ArrayList<User>();
-
-    String selectSQL = "SELECT * FROM " + DBUserDAO.TABLE_NAME;
-
+  @Override
+  public List<User> retrieveAll()  {
+    final String query = "SELECT * FROM " + DBUserDAO.TABLE_NAME;
     try {
-      connection = ConnectionPool.getConnection();
-      preparedStatement = connection.prepareStatement(selectSQL);
+      PreparedStatement stm = connection.prepareStatement(query);
+      stm.execute();
 
-      ResultSet rs = preparedStatement.executeQuery();
+      ResultSet rs = stm.getResultSet();
+      if (!rs.next()) {
+        return null;
+      }
+      List<User>  users = new ArrayList<User>();
 
       while (rs.next()) {
         User bean = new User();
@@ -149,97 +167,63 @@ public class DBUserDAO implements UserDAO{
         bean.setPassword(rs.getString("PASSWORD"));
         users.add(bean);
       }
-
-    } finally {
-      try {
-        if (preparedStatement != null) {
-          preparedStatement.close();
-        }
-
-      } finally {
-        ConnectionPool.releaseConnection(connection);
-      }
+      return users;
+    } catch (SQLException ex) {
+      logger.log(Level.SEVERE, ex.getMessage());
+      return null;
     }
-    return users;
   }
 
-  public synchronized void insert(User user) throws SQLException {
 
-    Connection connection = null;
-    PreparedStatement preparedStatement = null;
-
-    String insertSQL = "INSERT INTO " + DBUserDAO.TABLE_NAME
+  @Override
+  public void insert(User user) throws EntityTamperingException {
+    final String query = "INSERT INTO " + DBUserDAO.TABLE_NAME
         + " (ID, NOME, COGNOME, EMAIL, PASSWIRD) VALUES (?,?,?,?,?)";
 
     try {
-      connection = ConnectionPool.getConnection();
-      preparedStatement = connection.prepareStatement(insertSQL);
-      preparedStatement.setString(1, user.getId());
-      preparedStatement.setString(2, user.getName());
-      preparedStatement.setString(3, user.getSurname());
-      preparedStatement.setString(4, user.getEmail());
-      preparedStatement.setString(5, user.getPassword());
-      preparedStatement.executeUpdate();
-      connection.commit();
-    } finally {
-      try {
-        if (preparedStatement != null) {
-          preparedStatement.close();
-        }
-      } finally {
-        ConnectionPool.releaseConnection(connection);
-      }
+      PreparedStatement stm = connection.prepareStatement(query);
+      stm.setString(1, user.getId());
+      stm.setString(2, user.getName());
+      stm.setString(3, user.getSurname());
+      stm.setString(4, user.getEmail());
+      stm.setString(5, user.getPassword());
+      stm.executeUpdate();
+    } catch (SQLException ex) {
+      logger.log(Level.SEVERE, ex.getMessage());
+      throw new EntityTamperingException("Already existing Order!");
     }
   }
 
-  public synchronized void update(User bean) throws SQLException {
-
-    Connection connection = null;
-    PreparedStatement preparedStatement = null;
-
-    String updateSQL ="UPDATE "+ DBUserDAO.TABLE_NAME+" SET name = ?, surname = ?, email = ?, password = ? WHERE ID = ?";
+  @Override
+  public void update(User user) throws EntityTamperingException {
+    final String query = "UPDATE "+ DBUserDAO.TABLE_NAME+" SET name = ?, surname = ?, email = ?, password = ? WHERE ID = ?";
 
     try {
-      connection = ConnectionPool.getConnection();
-      preparedStatement = connection.prepareStatement(updateSQL);
-      preparedStatement.setString(1, bean.getName());
-      preparedStatement.setString(2, bean.getSurname());
-      preparedStatement.setString(3, bean.getEmail());
-      preparedStatement.setString(4, bean.getPassword());
-      preparedStatement.setString(5, bean.getId());
-      preparedStatement.executeUpdate();
+      PreparedStatement stm = connection.prepareStatement(query);
 
-    } finally {
-      try {
-        if (preparedStatement != null) {
-          preparedStatement.close();
-        }
-      } finally {
-        ConnectionPool.releaseConnection(connection);
-      }
+      stm.setString(1, user.getName());
+      stm.setString(2, user.getSurname());
+      stm.setString(3, user.getEmail());
+      stm.setString(4, user.getPassword());
+      stm.setString(5, user.getId());
+      stm.executeUpdate();
+    } catch (SQLException ex) {
+      logger.log(Level.SEVERE, ex.getMessage());
+      throw new EntityTamperingException("Already existing Order!");
     }
   }
 
-  public synchronized void delete(String id) throws SQLException {
-    Connection connection = null;
-    PreparedStatement preparedStatement = null;
-
-    String deleteSQL = "DELETE FROM " + DBUserDAO.TABLE_NAME + " WHERE ID = ?";
+  @Override
+  public void delete(String id) throws EntityTamperingException {
+    final String query = "DELETE FROM " + DBUserDAO.TABLE_NAME + " WHERE ID = ?";
 
     try {
-      connection = ConnectionPool.getConnection();
-      preparedStatement = connection.prepareStatement(deleteSQL);
-      preparedStatement.setString(1, id);
-      preparedStatement.executeUpdate();
-
-    } finally {
-      try {
-        if (preparedStatement != null) {
-          preparedStatement.close();
-        }
-      } finally {
-        ConnectionPool.releaseConnection(connection);
-      }
+      PreparedStatement stm = connection.prepareStatement(query);
+      stm.setString(1, id);
+      stm.executeUpdate();
+    } catch (SQLException ex) {
+      logger.log(Level.SEVERE, ex.getMessage());
+      throw new EntityTamperingException("Order does not exist!");
     }
   }
 
