3629a9e171d7d9594a177a5f1c9449e1b942931a
sara81278 <sara81278@users.noreply.github.com>
2021-01-14 19:54:07 +0100

    Modifica di ViewRequestEstimateServlet
    Nel metodo doPost non c'era niente.

--- Code/EasyLease/src/main/java/com/easylease/EasyLease/control/client/ViewRequestEstimateServlet.java
+++ Code/EasyLease/src/main/java/com/easylease/EasyLease/control/client/ViewRequestEstimateServlet.java
@@ -37,6 +37,6 @@ public class ViewRequestEstimateServlet extends HttpServlet {
   protected void doPost(
       HttpServletRequest request,
       HttpServletResponse response) throws ServletException, IOException {
-
+    doGet(request, response);
   }
 }
