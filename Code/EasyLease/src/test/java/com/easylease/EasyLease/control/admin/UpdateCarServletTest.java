5dcd0fce247068922e87291541225932328ed775
Michele-iodice <72759068+Michele-iodice@users.noreply.github.com>
2021-01-15 10:58:09 +0100

    aggiornato test della servlet UpdateCarServlet
    file versione 2
    modificate utilizzando il metodo scelto durante l'ultimo meeting

--- Code/EasyLease/src/test/java/com/easylease/EasyLease/control/admin/UpdateCarServletTest.java
+++ Code/EasyLease/src/test/java/com/easylease/EasyLease/control/admin/UpdateCarServletTest.java
@@ -4,7 +4,9 @@ import com.easylease.EasyLease.model.car.Car;
 import com.easylease.EasyLease.model.car.CarDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,17 +14,12 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
 class UpdateCarServletTest {
-
-
     private UpdateCarServlet servlet;
     private HttpServletRequest request;
     private HttpServletResponse response;
@@ -30,12 +27,17 @@ class UpdateCarServletTest {
     private CarDAO carDAO;
     private Car car;
     private StringWriter response_writer;
+    private RequestDispatcher dispatcher;
+    private ServletContext context;
+    private ServletConfig config;
 
 
 
     @BeforeEach
-    public void setUp() throws IOException {
+    public void setUp() throws IOException, ServletException {
         servlet = new UpdateCarServlet();
+        config =mock(ServletConfig.class);
+        servlet.init(config);
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
         response_writer = new StringWriter();
@@ -50,72 +52,33 @@ class UpdateCarServletTest {
         }).when(carDAO).insert(any());
         when(request.getSession()).thenReturn(session);
         when(response.getWriter()).thenReturn(new PrintWriter(response_writer));
-
+        context =mock(ServletContext.class);
+        dispatcher= mock(RequestDispatcher.class);
+        when(servlet.getServletContext()).thenReturn(context);
+        when(context.getRequestDispatcher(anyString())).thenReturn(dispatcher);
     }
 
 
     @Test
-    void testRoleNull() {
-
+    void testRoleNull() throws ServletException, IOException {
         when(request.getSession().getAttribute("role")).thenReturn(null);
-        when(request.getParameter("ID_Update")).thenReturn("CAAA111");
-        when(request.getParameter("brand_Update")).thenReturn("Mercedes");
-        when(request.getParameter("model_Update")).thenReturn("Classe E");
-        when(request.getParameter("doors_Update")).thenReturn("5");
-        when(request.getParameter("car_type_Update")).thenReturn("berlina");
-        when(request.getParameter("transmission_Update")).thenReturn("Automatico");
-        when(request.getParameter("avg_consumption_Update")).thenReturn("12");
-        when(request.getParameter("horse_power_Update")).thenReturn("200");
-        when(request.getParameter("emission_class_Update")).thenReturn("Euro 6");
-        when(request.getParameter("co2_emissions_Update")).thenReturn("113");
-        when(request.getParameter("power_supply_Update")).thenReturn("Diesel");
-        when(request.getParameter("capacity_Update")).thenReturn("2000");
-        when(request.getParameter("price_Update")).thenReturn("500");
-        when(request.getParameter("img_car_Update")).thenReturn("peugeot_3008.jpg");
 
-        assertThrows(IllegalStateException.class,()->{servlet.doPost(request,response);});
+        servlet.doGet(request,response);
+        verify(context).getRequestDispatcher("/fragments/error403.jsp");
     }
 
     @Test
-    void testRoleNotAdmin() {
-
+    void testRoleNotAdmin() throws ServletException, IOException {
         when(request.getSession().getAttribute("role")).thenReturn("client");
-        when(request.getParameter("ID_Update")).thenReturn("CAAA111");
-        when(request.getParameter("brand_Update")).thenReturn("Mercedes");
-        when(request.getParameter("model_Update")).thenReturn("Classe E");
-        when(request.getParameter("doors_Update")).thenReturn("5");
-        when(request.getParameter("car_type_Update")).thenReturn("berlina");
-        when(request.getParameter("transmission_Update")).thenReturn("Automatico");
-        when(request.getParameter("avg_consumption_Update")).thenReturn("12");
-        when(request.getParameter("horse_power_Update")).thenReturn("200");
-        when(request.getParameter("emission_class_Update")).thenReturn("Euro 6");
-        when(request.getParameter("co2_emissions_Update")).thenReturn("113");
-        when(request.getParameter("power_supply_Update")).thenReturn("Diesel");
-        when(request.getParameter("capacity_Update")).thenReturn("2000");
-        when(request.getParameter("price_Update")).thenReturn("500");
-        when(request.getParameter("img_car_Update")).thenReturn("peugeot_3008.jpg");
 
-        assertThrows(IllegalStateException.class,()->{servlet.doPost(request,response);});
+        servlet.doGet(request,response);
+        verify(context).getRequestDispatcher("/fragments/error403.jsp");
     }
 
     @Test
     void testIDUpdateNull() throws ServletException, IOException {
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn(null);
-        when(request.getParameter("brand_Update")).thenReturn("Mercedes");
-        when(request.getParameter("model_Update")).thenReturn("Classe E");
-        when(request.getParameter("doors_Update")).thenReturn("5");
-        when(request.getParameter("car_type_Update")).thenReturn("berlina");
-        when(request.getParameter("transmission_Update")).thenReturn("Automatico");
-        when(request.getParameter("avg_consumption_Update")).thenReturn("12");
-        when(request.getParameter("horse_power_Update")).thenReturn("200");
-        when(request.getParameter("emission_class_Update")).thenReturn("Euro 6");
-        when(request.getParameter("co2_emissions_Update")).thenReturn("113");
-        when(request.getParameter("power_supply_Update")).thenReturn("Diesel");
-        when(request.getParameter("capacity_Update")).thenReturn("2000");
-        when(request.getParameter("price_Update")).thenReturn("500");
-        when(request.getParameter("img_car_Update")).thenReturn("peugeot_3008.jpg");
-
         when(response.getContentType()).thenReturn("text/html;charset=UTF-8");
 
         servlet.doPost(request, response);
@@ -127,19 +90,6 @@ class UpdateCarServletTest {
     void testIDUpdateEmpty() throws ServletException, IOException {
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("");
-        when(request.getParameter("brand_Update")).thenReturn("Mercedes");
-        when(request.getParameter("model_Update")).thenReturn("Classe E");
-        when(request.getParameter("doors_Update")).thenReturn("5");
-        when(request.getParameter("car_type_Update")).thenReturn("berlina");
-        when(request.getParameter("transmission_Update")).thenReturn("Automatico");
-        when(request.getParameter("avg_consumption_Update")).thenReturn("12");
-        when(request.getParameter("horse_power_Update")).thenReturn("200");
-        when(request.getParameter("emission_class_Update")).thenReturn("Euro 6");
-        when(request.getParameter("co2_emissions_Update")).thenReturn("113");
-        when(request.getParameter("power_supply_Update")).thenReturn("Diesel");
-        when(request.getParameter("capacity_Update")).thenReturn("2000");
-        when(request.getParameter("price_Update")).thenReturn("500");
-        when(request.getParameter("img_car_Update")).thenReturn("peugeot_3008.jpg");
         when(response.getContentType()).thenReturn("text/html;charset=UTF-8");
 
         servlet.doPost(request, response);
@@ -255,7 +205,6 @@ class UpdateCarServletTest {
 
     @Test
     void testCarAverageNull() {
-
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("CAAA111");
         when(request.getParameter("brand_Update")).thenReturn("Mercedes");
@@ -277,7 +226,6 @@ class UpdateCarServletTest {
 
     @Test
     void testCarHorsePowerNull() {
-
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("CAAA111");
         when(request.getParameter("brand_Update")).thenReturn("Mercedes");
@@ -299,7 +247,6 @@ class UpdateCarServletTest {
 
     @Test
     void testCarEmissionClassNull() {
-
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("CAAA111");
         when(request.getParameter("brand_Update")).thenReturn("Mercedes");
@@ -322,7 +269,6 @@ class UpdateCarServletTest {
 
     @Test
     void testCarCo2EmissionsNull() {
-
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("CAAA111");
         when(request.getParameter("brand_Update")).thenReturn("Mercedes");
@@ -345,7 +291,6 @@ class UpdateCarServletTest {
 
     @Test
     void testCarPowerSupplyNull()  {
-
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("CAAA111");
         when(request.getParameter("brand_Update")).thenReturn("Mercedes");
@@ -369,7 +314,6 @@ class UpdateCarServletTest {
 
     @Test
     void testCarCapacityNull()  {
-
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("CAAA111");
         when(request.getParameter("brand_Update")).thenReturn("Mercedes");
@@ -393,7 +337,6 @@ class UpdateCarServletTest {
 
     @Test
     void testCarPriceNull()  {
-
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("CAAA111");
         when(request.getParameter("brand_Update")).thenReturn("Mercedes");
@@ -416,7 +359,6 @@ class UpdateCarServletTest {
 
     @Test
     void testCarImageNull()  {
-
         when(request.getSession().getAttribute("role")).thenReturn("admin");
         when(request.getParameter("ID_Update")).thenReturn("CAAA111");
         when(request.getParameter("brand_Update")).thenReturn("Mercedes");
