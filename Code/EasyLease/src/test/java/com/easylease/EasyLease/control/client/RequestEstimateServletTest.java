4cfef6df44711945a63b5b0dd6162396ebbd9d14
sara81278 <sara81278@users.noreply.github.com>
2021-01-30 09:36:05 +0100

    Cambio password DB dei test
    in RequestEstimateServletTest, ViewRequestEstimateServletTest, HomePageServletTest, ResearchServletTest

--- Code/EasyLease/src/test/java/com/easylease/EasyLease/control/client/RequestEstimateServletTest.java
+++ Code/EasyLease/src/test/java/com/easylease/EasyLease/control/client/RequestEstimateServletTest.java
@@ -56,7 +56,7 @@ class RequestEstimateServletTest {
     MysqlDataSource mysqlDataSource = new MysqlDataSource();
     mysqlDataSource.setURL("jdbc:mysql://localhost:3306/easylease");
     mysqlDataSource.setUser("root");
-    mysqlDataSource.setPassword("master");
+    mysqlDataSource.setPassword("root");
     mysqlDataSource.setServerTimezone("UTC");
     mysqlDataSource.setVerifyServerCertificate(false);
     mysqlDataSource.setUseSSL(false);
